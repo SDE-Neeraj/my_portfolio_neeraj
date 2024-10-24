51fdbad081700301d94ca0ade355ca8e2f95687a
SDE-Neeraj <mail.singhneeraj@gamil.com>
2024-10-25 01:35:29 +0530

    Update in home section

---
+++ script.js
@@ -0,0 +1,14 @@
+const text = "I'm Neeraj Singh, Architect & Software Developer.";
+const typewriterElement = document.getElementById('typewriter');
+let index = 0;
+
+function type() {
+    if (index < text.length) {
+        typewriterElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100); // Adjust speed by changing this value
+    }
+}
+
+// Start typing effect after a short delay
+setTimeout(type, 100); // Adjust the start delay as needed
